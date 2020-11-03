edf73bca66ef1b8fb6be17d8fbdfe5abbf3c9b86
Elama12 <73789682+Elama12@users.noreply.github.com>
2020-11-03 21:53:30 +0530

    Update first.js

--- first.js
+++ first.js
@@ -2,3 +2,12 @@ var x, y, z;  // Statement 1
 x = 5;        // Statement 2
 y = 6;        // Statement 3
 z = x + y;    // Statement 4
+var a,b,c
+a=10
+b= 20
+c= a+b
+console.log(c)
+e=a*b
+console.log(e)
+n=a-b
+console.log(n)

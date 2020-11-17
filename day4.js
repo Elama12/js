4903fc2888eeb450e4489001466d0e763d067818
Elama12 <73789682+Elama12@users.noreply.github.com>
2020-11-17 23:03:06 +0530

    ok

--- day4.js
+++ day4.js
@@ -1,6 +1,6 @@
-// let explicit = "3.5"
-// console.log(explicit+" typeof"+typeof(324e-1))
-// explicit= Number(explicit)
+let explicit = 3.5
+console.log(explicit+" typeof"+typeof(324e-1))
+explicit= Number(explicit)
 // console.log(explicit+" typeof"+typeof(explicit))
 // STRING TO NUMBER CONVERSION WITH HELP OF NUMBER (INBUILD METHOD)FUNCTION
 // let parint=parseInt("3.5")
@@ -13,36 +13,3 @@
 // control flow
 // let number=-4
 
-// if(number%2==0){
-//     console.log("even")
-
-//     if (number>0){
-//         console.log("the number is positive")
-//     }
-//     else{
-//         console.log("the number is negative")
-//     }
-// }
-
-// else
-// {
-//     console.log("odd")
-
-// }
-
-
-let name="varun"
-let pin =2 
-
-let input_name="varun"
-let intput_pin=1
- if(input_name == name){
-     if(intput_pin == pin){
-         console.log("hi varun")
-     }
-     else{
-        console.log("invalid pin ")
-     }
- }else{
-     console.log("invalid username")
- }
---
+++ day5.js
@@ -0,0 +1,28 @@
+// // program 1
+// let num = 2 
+//  if(num%2==0){
+//      console.log(" 2 is a even number")
+//  }
+//  else{
+//      console.log("odd number")
+//  }
+
+// program 2
+
+let name="varun" 
+let pin = 344
+
+let inputname=varun
+let inputpin=344
+if(inputname=name){
+    console.log("the name is varun")
+}
+if(inputpin=pin){
+    console.log("the pin is correct")
+}else if{
+console.log("invalid username")
+}
+else {
+    console.log('invalid username')
+}
+}

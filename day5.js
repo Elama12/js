8446a01ae7ef064a6d9fc8c3ee45af578d3144c5
Elama12 <73789682+Elama12@users.noreply.github.com>
2020-11-18 21:36:33 +0530

    day6

--- day5.js
+++ day5.js
@@ -9,20 +9,24 @@
 
 // program 2
 
-let name="varun" 
-let pin = 344
+//  let number=2
+//  if(number>=0){
+//      if(number==0){
+//          console.log("hero");
+//      }else{
+//          console.log("villain");
+//      }
+//  }else{
+//      console.log("badboy");
+//  }
+ 
 
-let inputname=varun
-let inputpin=344
-if(inputname=name){
-    console.log("the name is varun")
-}
-if(inputpin=pin){
-    console.log("the pin is correct")
-}else if{
-console.log("invalid username")
-}
-else {
-    console.log('invalid username')
-}
+// loop
+// let number =2
+// for(let  i=1;i<=2;i++){
+//     console.log("maran podhu")
+// }
+let n=4
+for(let i=1;i<=n;i++){
+    console.log(i)
 }
---
+++ day6.js
@@ -0,0 +1,5 @@
+// loop 
+let number=5
+for(let i=1;i<=name;i++){
+    console.log("maranpodhu")
+}
